bde0d5bd171aebf169478c8ee2ed87dde5e6af1e
Rakshitha037 <rakshitharakshitha6242@gmail.com>
2023-07-09 17:47:31 +0000

    Changes

--- RR.c
+++ RR.c
@@ -1,5 +1,5 @@
 #include<stdio.h>  
-#include<conio.h>
+
 void main()  
 {  
     // initlialize the variable name  
@@ -63,5 +63,5 @@ avg_wt = wt * 1.0/NOP;
 avg_tat = tat * 1.0/NOP;  
 printf("\n Average Turn Around Time: \t%f", avg_wt);  
 printf("\n Average Waiting Time: \t%f", avg_tat);  
-getch();  
+  
 }
